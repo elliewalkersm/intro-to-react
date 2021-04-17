1d8ea9c107f68fe247c6a0f1a3b277999e4f342a
elliewalkersm <elliewalkersm@gmail.com>
2021-04-17 13:47:25 -0500

    added props array

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@ import './App.css';
 import Counter from './Counter';
 
 function App() {
+  const array = [
+    {
+      appName: "Counter 1",
+      counterStart: 200
+    },
+    {
+      appName: "Counter 2",
+      counterStart: 200
+    },
+    {
+      appName: "Counter 3",
+      counterStart: 200
+    }
+  ];
+
   return (
     <>
-      <Counter appName="Counter 1" counterStart={200}/>
-      <Counter appName="Counter 2" counterStart={200}/>
-      <Counter appName="Counter 3" counterStart={200}/>
+      {array.map((counter, i) => 
+        <Counter  key={i} appName={counter.appName} counterStart={counter.counterStart}/>
+      )};
     </>
   )
 }
